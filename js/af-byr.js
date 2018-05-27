1ef17010f1fc0d702c5465398dcbfd03aa20ba93
raldo <raldo0104@gmail.com>
2018-05-27 14:46:09 +0700

    wait

--- js/af-byr.js
+++ js/af-byr.js
@@ -7,6 +7,8 @@ $.ajax({url: "byr-re.php", success: function(result){
    	    console.log(result);
    	    $("#theb").html("");
    	    for (var i = data.length - 1; i >= 0; i--) {
+          total = data[i].quantity*data[i].price;
+
    	    	var dom='<tr>\
           <td style="text-align:right;border-bottom:#F0F0F0 1px solid; color: black">'+data[i].order_id+'</td>\
                     <td style="text-align:right;border-bottom:#F0F0F0 1px solid; color: black">'+data[i].person_id+'</td>\
@@ -14,7 +16,7 @@ $.ajax({url: "byr-re.php", success: function(result){
                     <td style="text-align:left;border-bottom:#F0F0F0 1px solid; color: black"><strong>'+data[i].Nama+'</strong></td>\
                     <td style="text-align:right;border-bottom:#F0F0F0 1px solid; color: black">'+data[i].order_date+'</td>\
                     <td style="text-align:left;border-bottom:#F0F0F0 1px solid; color: black"><strong>'+data[i].quantity+'</strong></td>\
-                    <td style="text-align:right;border-bottom:#F0F0F0 1px solid; color: black"><strong style="color: black"></strong>'+data[i].quantity+*+data[i].price+'rb</td>\
+                    <td style="text-align:right;border-bottom:#F0F0F0 1px solid; color: black"><strong style="color: black"></strong>'+total+'rb</td>\
                     <td style="text-align:center;border-bottom:#F0F0F0 1px solid;"><a id="btnEmpty" href="bayar.php?order_id='+data[i].order_id+'&product_id='+data[i].product_id+'" class="btnRemoveAction">Dibayar</a></td>\
                     </tr>';
                  $("#theb").append(dom);
