822c40fc338086cca0217eadb5a68c68a5c9d352
Khawari Muhammad Dzakwan <aridzakwan@gmail.com>
2018-05-27 14:45:07 +0700

    CRUD

--- update.php
+++ update.php
@@ -1,23 +1,22 @@
 <?php
-$con = mysqli_connect('localhost','root','');
-mysqli_select_db($con,'PWEB182_E_16088');
-
+include("_con.php");
+$db_update = new DBController();
 // cek apakah tombol simpan sudah diklik atau blum?
 if(isset($_POST['simpan'])){
 
     // ambil data dari formulir
     $id = $_POST['id'];
-    $username = $_POST['username'];
-    $apassword = md5($_POST['password']);
-
+    $name = $_POST['name'];
+    $price = $_POST['price'];
+    $jenis = $_POST['jenis'];
     // buat query update
-    $sql = "UPDATE login SET username='$username', password='$password' WHERE id=$id";
-    $query = mysqli_query($con, $sql);
+    $sql = "CALL sp_edit('$id','$name','$price','$jenis')";
+    $query = $db_update->runQuery($sql);
 
     // apakah query update berhasil?
     if( $query ) {
         // kalau berhasil alihkan ke halaman list-siswa.php
-        header('Location: user.php');
+        header('Location: user.php?action=update');
     } else {
         // kalau gagal tampilkan pesan
         die("Gagal menyimpan perubahan...");
